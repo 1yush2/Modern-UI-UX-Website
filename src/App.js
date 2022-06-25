a7758ee7b24f3139fe0cd083a06be61ca32a0b12
Aayush Karki <1yush225@gmail.com>
2022-06-26 02:11:01 +0545

    feat: added folder structring for the project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { Navbar } from "./components";
+import { Blogs } from "./containers";
+
 function App() {
   return (
-    <h1>Responsive UI/UX website</h1>
+    <>
+      <h1>Responsive UI/UX website</h1>
+      <Navbar />
+      <Blogs />
+    </>
   );
 }
 
---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export { default as Blogs } from "./blogs/Blogs";
+export { default as Features } from "./features/Features";
+export { default as Footer } from "./footer/Footer";
+export { default as Header } from "./header/Header";
+export { default as WhatGpt3 } from "./whatGPT3/WhatGpt3";
+
