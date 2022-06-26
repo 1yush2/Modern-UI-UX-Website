5c7be5eaed2de131aa3ad94cf970325c8cdd209e
Aayush Karki <1yush225@gmail.com>
2022-06-26 15:07:46 +0545

    feat: created blogs, features and footer page

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import gpt3Logo from "../../assets/logo.svg";
+import "./footer.css";
 
-const Footer = () => {
+const FooterLinks = ({ subtitle, link }) => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <h4>{subtitle}</h4>
+      {link.map((linkData, index) => {
+        return <p key={index}>{linkData}</p>;
+      })}
+    </>
+  );
+};
 
-export default Footer
+const Footer = () => (
+  <div className="gpt3__footer section__padding">
+    <div className="gpt3__footer-heading">
+      <h1 className="gradient__text">
+        Do you want to step in to the future before others
+      </h1>
+    </div>
+
+    <div className="gpt3__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links_logo">
+        <img src={gpt3Logo} alt="gpt3_logo" />
+        <p>
+          Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved
+        </p>
+      </div>
+
+      <div className="gpt3__footer-links_div">
+        <FooterLinks
+          subtitle="Links"
+          link={["Overons", "Social Media", "Counters", "Contact"]}
+        />
+      </div>
+
+      <div className="gpt3__footer-links_div">
+        <FooterLinks
+          subtitle="Company"
+          link={["Terms & Conditions", "Privacy Policy", "Contact"]}
+        />
+      </div>
+
+      <div className="gpt3__footer-links_div">
+        <FooterLinks
+          subtitle="Get in touch"
+          link={["Kathmandu K12 182 DK 99", "9828172288", "info@payMe.net"]}
+        />
+      </div>
+    </div>
+
+    <div className="gpt3__footer-copyright">
+      <p>@2021 GPT-3. All rights reserved.</p>
+    </div>
+  </div>
+);
+
+export default Footer;
